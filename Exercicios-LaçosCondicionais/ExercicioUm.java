58b8dda1e5e905a865f5a183436ec8265ac8f86b
nathaliacrz <nathalias161@gmail.com>
2022-05-30 10:24:08 -0300

    Criando nova pasta e adicionando exercicio 1

---
+++ Exercicios-LaçosCondicionais/ExercicioUm.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioUm {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double multa;
+		int excesso;
+		
+		System.out.println("Insira peso total dos tomates: ");
+		int pesoTomates = sc.nextInt(); 
+		
+		if(pesoTomates > 50) {
+			excesso = pesoTomates - 50;
+			multa = excesso * 4;
+			System.out.printf("Houve excesso de %d kg de tomate. Multa pelo excesso: R$%.2f.", excesso,  multa);
+		} else if(pesoTomates <= 50) {
+			System.out.println("Nao houve excesso. Pagamento extra = R$0.0");
+		}
+		
+		sc.close();
+	}
+}
