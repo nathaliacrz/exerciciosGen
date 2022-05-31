d3ccd13bba835dcc20c27f0eb57d58252c9826eb
nathaliacrz <nathalias161@gmail.com>
2022-05-31 12:32:56 -0300

    Adicionando exercicio 3

---
+++ ExerciciosLista3/WhileExercicio1.java
@@ -0,0 +1,39 @@
+package laçosRepeticao;
+
+import java.util.Scanner;
+
+public class WhileExercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		double soma, media;
+		int totalValores;
+		
+		soma = 0;
+		totalValores = 0;
+		
+		System.out.println("Insira um valor: ");
+		double valor = sc.nextDouble();
+		
+		while(valor > 0) {
+			totalValores++;
+			soma += valor;
+			System.out.println("Insira outro valor: ");
+			valor = sc.nextDouble();
+		}
+		
+		media = soma / totalValores;
+		
+		System.out.println("Valores lidos: " + totalValores);
+		System.out.println("Soma: " + soma);
+		System.out.println("Media: " + media);
+		
+		sc.close();
+	
+	}
+	
+	
+	
+}
