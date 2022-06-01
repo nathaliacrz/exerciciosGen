0b046d698bf5f6189a60a1570642340d7796ec51
nathaliacrz <nathalias161@gmail.com>
2022-06-01 12:30:27 -0300

    Adicionando lista 4 e Exercicio 1

---
+++ ExerciciosLista4/ExercicioVetor1.java
@@ -0,0 +1,29 @@
+package vetores;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Insira 5 valores: ");
+
+		double[] valor = new double[4];
+		double maior = 0;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Valor " + (i + 1) + ":");
+			valor[i] = sc.nextDouble();
+
+			if (valor[i] > maior) {
+				maior = valor[i];
+			}
+
+			System.out.println(maior);
+			sc.close();
+
+		}
+	}
+}
