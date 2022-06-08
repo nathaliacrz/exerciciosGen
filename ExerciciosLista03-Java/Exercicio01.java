eae492b34ffd65b646d057728fb8c580c4f36805
nathaliacrz <nathalias161@gmail.com>
2022-06-08 11:04:15 -0300

    Alterando nome das pastas Java e adicionando lista 03

---
+++ ExerciciosLista03-Java/Exercicio01.java
@@ -0,0 +1,13 @@
+package novaLista;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+			for(int numero = 1000; numero <= 1999; numero++) {
+				if(numero % 11 == 5) {
+					System.out.println(numero);
+				}
+			}
+	}
+}
